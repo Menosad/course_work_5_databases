e69d5221c0775e74245e59ea5ec5a6ca6e8ff28d
menosad <draignair@gmail.com>
2024-05-14 17:41:20 +0300

    #2 rework func loading_data_into_database

--- src/utils.py
+++ src/utils.py
@@ -66,18 +66,20 @@ def upload_to_database(data_list):
 
 
 class DBManager:
-    __slots__ = ['final_data']
+    __slots__ = ['final_data', 'vacancies_list']
 
     def __init__(self):
         self.final_data = []
+        self.vacancies_list = []
 
     def get_companies_and_vacancies_count(self):
         """получает список всех компаний и количество вакансий у каждой компании"""
+        print(f"Собираю данные по Вашему запросу...")
         headers = {'User-Agent': 'HH-User-Agent'}
         url = 'https://api.hh.ru/vacancies'
-        for num, i in enumerate(range(19)):
-            params = {'per_page': 110, 'page': i}
-            response = requests.get(url, headers=headers, params=params)
+        for i in range(19):
+            params = {'page': i + 1, 'per_page': 110}
+            response = requests.get(url, headers=headers, params={'page': i, 'per_page': 100})
             vacancies_list = json.loads(response.text)['items']
             self.final_data.extend(vacancies_list)
         return self.final_data, len(self.final_data)
@@ -85,9 +87,10 @@ class DBManager:
     def get_all_vacancies(self):
         """получает список всех вакансий с указанием названия компании,
         названия вакансии и зарплаты и ссылки на вакансию"""
-        vacancies_list = []
+        print(f"Компаную данные...")
         for vacancy in self.final_data:
-            vac_dict = {'company': vacancy['employer']['name'], 'name': vacancy['name'], 'url': vacancy['alternate_url']}
+            vac_dict = {'company': vacancy['employer']['name'], 'name': vacancy['name'],
+                        'url': vacancy['alternate_url']}
             if vacancy.get('salary'):
                 vac_dict['from'] = vacancy.get('salary').get('from')
                 vac_dict['to'] = vacancy.get('salary').get('to')
@@ -96,16 +99,46 @@ class DBManager:
                 vac_dict['from'] = vacancy.get('salary')
                 vac_dict['to'] = vacancy.get('salary')
                 vac_dict['currency'] = vacancy.get('salary')
-            vacancies_list.append(vac_dict)
-        return vacancies_list
+            self.vacancies_list.append(vac_dict)
+        return self.vacancies_list
 
+    def loading_data_into_database(self):
+        create_table = (f"CREATE TABLE IF NOT EXISTS vacancies"
+                        f"(id serial PRIMARY KEY,"
+                        f"name varchar(50) NOT NULL,"
+                        f"salary_from int,"
+                        f"salary_to int,"
+                        f"currency varchar(10),"
+                        f"company varchar(50) NOT NULL,"
+                        f"url varchar(100))")
+        drop_table = f"DROP TABLE IF EXISTS vacancies"
+        params = config()
+        insert_data = (f"INSERT INTO vacancies (name, salary_from, salary_to, currency, company, url) VALUES (%s, %s, "
+                       f"%s, %s, %s, %s, %s)")
+        with psycopg2.connect(**params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(drop_table)
+                cur.execute(create_table)
+            conn.commit()
+            with conn.cursor() as cur:
+                for vac in self.vacancies_list:
+                    cur.execute(insert_data, (vac['name'], vac['salary_from'], vac['salary_to'], vac['currency'],
+                                              vac['company'], vac['url']))
+            conn.commit()
+            with conn.cursor() as cur:
+                cur.execute(f"SELECT * FROM vacancies")
+                rows = cur.fetchall()
+                for row in rows:
+                    print(row)
 
     def get_avg_salary(self):
         """получает среднюю зарплату по вакансиям"""
         pass
+
     def get_vacancies_with_higher_salary(self):
         """получает список всех вакансий, у которых зарплата выше средней по всем вакансиям"""
         pass
+
     def get_vacancies_with_keyword(self):
         """получает список всех вакансий, в названии которых содержатся переданные в метод слова,
          например python"""
@@ -115,4 +148,4 @@ class DBManager:
 dbmanager = DBManager()
 dbmanager.get_companies_and_vacancies_count()
 data = dbmanager.get_all_vacancies()
-print(data[0])
+dbmanager.loading_data_into_database()
